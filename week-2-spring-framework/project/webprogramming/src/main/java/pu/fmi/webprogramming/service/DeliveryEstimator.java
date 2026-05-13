4f22559bd046b178648e08a3331d2651592bcf42
dido1043 <dudunovski_05@abv.bg>
2026-05-13 18:20:26 +0300

    Implement delivery time estimation logic and finalize constructor injection in DeliveryService

--- week-2-spring-framework/project/webprogramming/src/main/java/pu/fmi/webprogramming/service/DeliveryEstimator.java
+++ week-2-spring-framework/project/webprogramming/src/main/java/pu/fmi/webprogramming/service/DeliveryEstimator.java
@@ -9,26 +9,11 @@ import java.time.LocalDateTime;
 public class DeliveryEstimator {
 
   public LocalDateTime estimateArrivalTime(Delivery delivery) {
+      int days = delivery.getCustomer().getCity().equals(delivery.getWarehouse().getCity()) ? 1 : 3;
+      if (delivery.getCourier() == null) {
+          days += 2;
+      }
+      return delivery.getCreatedAt().plusDays(days);
 
-    // В Delivery е добавено ново поле LocalDateTime estimatedArrivalAt
-    // Този метод се използва в createDelivery на DeliveryService
-
-    // TODO: Довършване на имплементацията за изчисляване на очаквана дата на доставка
-    // * Провери дали градовете на склада и клиента съвпадат:
-    //   → При съвпадащи градове:
-    //     - очаквана дата на доставка трябва да е 1 ден след датата на създаване на доставка
-    //   → При различни градове между склада и този на клиента:
-    //     - очаквана дата на доставка трябва да е 3 дни след датата на създаване на доставка
-    //
-    // * Провери и наличността на куриер:
-    //   → Ако няма назначен куриер:
-    //     - Добави още 2 дни закъснение към вече изчислената дата
-    //
-    // ВАЖНО:
-    // * Всички предоставени Unit тестове (DeliveryServiceTest) трябва да минават успешно
-    // * Не променяйте тестовете
-    // * Не променяйте сигнатурата на метода
-
-    return null;
   }
 }
